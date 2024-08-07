47475286f3bbce3086d47766bacfeed39ffcebd1
Koushik Jana <128731535+KOUSHIKJANA007@users.noreply.github.com>
2024-08-07 20:20:43 +0530

    Update App.jsx
    small change

--- myntra-react-clone/src/routes/App.jsx
+++ myntra-react-clone/src/routes/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
-import FetchItems from "../components/fetchItems"
+import FetchItems from "../components/FetchItems"
 import { useSelector } from "react-redux"
 import LoadingSpinner from "../components/LoadingSpinner"
 import "bootstrap/dist/css/bootstrap.min.css"
